7cbfd74b0e6b7117f7a1364b5c8f848236727fcf
Gianluca Pepe <37369540+gianluca-pepe@users.noreply.github.com>
2021-11-08 10:43:33 +0100

    basic api usage

---
+++ src/highlight-style.js
@@ -0,0 +1,5 @@
+export default {
+  'underlay-color' : 'red',
+  'underlay-padding' : '10px',
+  'underlay-opacity' : 0.2,
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,61 @@
 import GrapholScape from 'grapholscape'
+import Ontology from 'grapholscape/src/model/ontology';
+import {getHighlights} from './api/api_stub'
+import highlightStyle from './highlight-style';
 
 export default function useGrapholscape(file) {
   document.getElementById('home').style.display = 'none';
   grapholscape_container.style.display = 'block';
 
-  let g = new GrapholScape(file, grapholscape_container).then( grapholscape => grapholscape.showDiagram(0))
+  let g = new GrapholScape(file, grapholscape_container).then( grapholscape => {
+    grapholscape.showDiagram(0)
+
+    sparqling(grapholscape)
+  })
+}
+
+
+function sparqling( grapholscape ) {
+  let cy = grapholscape.view.renderer.cy
+
+  
+  /**
+   * @type {Ontology}
+   */
+  let ontology = grapholscape.ontology
+
+  cy.on('select', 'node', e => {
+    let clickedIRI = e.target.data('iri').full_iri
+    let highlights
+    console.log(clickedIRI)
+    try{
+      highlights = getHighlights(ontology.name, ontology.version, clickedIRI)
+    } catch {
+      grapholscape.view.showDialog('warning', 'Wrong entity')
+    }
+
+    highlights?.selectedClasses.forEach( iri => selectIRI(iri))
+    highlights?.highlightedEntities.classes.forEach(iri => highlightIRI(iri))
+    highlights?.highlightedEntities.dataProperties.forEach( iri => highlightIRI(iri))
+    highlights?.highlightedEntities.objectProperties.map( o => o.objectPropertyIRI).forEach(iri => {
+      highlightIRI(iri)
+    })
+    cy.$('.highlighted').style(highlightStyle)
+  })
+
+
+  function highlightIRI(iri) {
+    //let diagramID = grapholscape.view.actual_diagram_id
+
+    let nodes = ontology.getOccurrences(iri)
+    nodes.forEach( n => { cy.getElementById(n.data.id).addClass('highlighted') })
+  }
+
+  function selectIRI(iri) {
+    //let diagramID = grapholscape.view.actual_diagram_id
+
+    let nodes = ontology.getOccurrences(iri)
+
+    nodes.forEach( n => cy.getElementById(n.data.id).select())
+  }
 }
