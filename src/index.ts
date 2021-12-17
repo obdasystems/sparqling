1356a89870ef20b4c64ffa9d62c89186a6fc4c51
gianluca-pepe <37369540+gianluca-pepe@users.noreply.github.com>
2021-12-17 10:49:59 +0100

    use axios-generated-api temp

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,28 @@ import { fullGrapholscape } from 'grapholscape'
 //import { getHighlights, getQueryGraphNode, putQueryGraphNodeDataProperty, putQueryGraphNodeObjectProperty } from './api/api_stub'
 import highlightStyle from './style/highlight-style'
 import QueryGraphRenderer from './query-graph/renderer'
-import { QueryGraphApi, OntologyGraphApi } from './api/swagger/api'
+import { QueryGraphApi, OntologyGraphApi, StandaloneApi } from './api/swagger/api'
 import * as containers from './get-container'
+import { fileToString } from './util'
+import { QueryGraph, GraphElement} from './api/swagger/models'
 
 export default async function sparqling(sparqlingContainer: HTMLDivElement, file: string | Blob) {
   for (const key in containers) { sparqlingContainer.appendChild(containers[key]) }
 
   const gscape = await fullGrapholscape(file, containers.grapholscape, { owl_translator: false })
   const qgRenderer = new QueryGraphRenderer(containers.sparqlingQueryGraph)
-  new OntologyGraphApi().highligths('test')
-  const qgApi = new QueryGraphApi()
   
+  const ogApi = new OntologyGraphApi()
+  const qgApi = new QueryGraphApi()
+  const soApi = new StandaloneApi()
+
+  let qg: QueryGraph
+
+  if (typeof file !== 'string')
+    file = await fileToString(file)
+  
+  soApi.standaloneOntologyUploadPost(file)
+
   gscape.showDiagram(0)
   setHighlightedStylesheet(gscape.renderer.cy, highlightStyle)
   gscape.onDiagramChange(() => setHighlightedStylesheet(gscape.renderer.cy, highlightStyle))
@@ -26,7 +37,7 @@ export default async function sparqling(sparqlingContainer: HTMLDivElement, file
   gscape.onEntitySelection( async (cyEntity: any) => {
     let clickedIRI: string = cyEntity.data('iri').prefixed
     resetHighlights(gscape.renderer.cy)
-    let newQueryGraph: any
+    let newQueryGraph: QueryGraph
 
     switch (cyEntity.data('type')) {
       case '':
@@ -41,6 +52,15 @@ export default async function sparqling(sparqlingContainer: HTMLDivElement, file
         if (clickedIRI !== qgRenderer.selectedGraphNode?.entity.iri) {
           try {
             if (lastObjProperty) {
+              newQueryGraph = await qgApi.putQueryGraphObjectProperty(
+                qg,
+                qgRenderer.selectedGraphNode.entity.iri,
+                lastObjProperty.data('iri').prefixed,
+                clickedIRI,
+                true,
+                qgRenderer.selectedGraphNode.id
+              )
+
               newQueryGraph = putQueryGraphNodeObjectProperty(null, null,
                 qgRenderer.selectedGraphNode.id, // graphNodeId
                 null, // sourceClassIri
@@ -54,8 +74,9 @@ export default async function sparqling(sparqlingContainer: HTMLDivElement, file
           } catch (error) { console.error(error) }
         }
         */
-        newQueryGraph = await qgApi.getQueryGraph(clickedIRI)
-        console.log(newQueryGraph)
+        qgApi.getQueryGraph(clickedIRI).then(newQueryGraph => {
+          console.log(newQueryGraph)
+        })
         //highlightSuggestions(clickedIRI)
         lastObjProperty = null
         break
@@ -110,9 +131,12 @@ export default async function sparqling(sparqlingContainer: HTMLDivElement, file
         // TODO show popup dialog
       } else if (objPropertyFromApi.relatedClasses.length === 1) {
 
-        let connectedClasses = objProperty.isEdge() ?
-          objProperty.connectedNodes('.concept') :
-          objProperty.neighborhood('node').neighborhood('node.concept')
+        // if it's not and edge, connected classes are attached to domain/range
+        // nodes
+        let connectedClasses = objProperty.isEdge() 
+          ? objProperty.connectedNodes('.concept')
+          // TODO: be sure first neighborhood only select range/domain restrictions node
+          : objProperty.neighborhood('node').neighborhood('node.concept')
 
         for (const i in connectedClasses) {
           if (connectedClasses[i].selected()) continue
