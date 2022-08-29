1d6fe4e7fe268515bb7d3d1e55a01abdf5d041b7
gianluca-pepe <37369540+gianluca-pepe@users.noreply.github.com>
2022-08-29 17:46:47 +0200

    Hide/show prefixes in query code

--- src/widgets/sparql-dialog.ts
+++ src/widgets/sparql-dialog.ts
@@ -1,6 +1,6 @@
 import { ui } from 'grapholscape'
 import { html, css, LitElement, PropertyValueMap } from 'lit'
-import { code, copyContent } from './assets/icons'
+import { code, copyContent, ellipsis } from './assets/icons'
 import { emptyQueryMsg } from './assets/texts'
 import sparqlingWidgetStyle from './sparqling-widget-style'
 import getTrayButtonTemplate from './tray-button-template'
@@ -9,6 +9,8 @@ export default class SparqlDialog extends ui.BaseMixin(LitElement) {
   text: string = emptyQueryMsg()
   //copyButton = new UI.GscapeButton(copyContent, "Copy Query")
   title = 'SPARQL'
+
+  private arePrefixesVisible: Boolean = false
 
   static styles = [
     ui.baseStyle,
@@ -20,19 +22,30 @@ export default class SparqlDialog extends ui.BaseMixin(LitElement) {
         left: 50%;
         transform: translate(-50%, 0);
         min-width: 200px;
+        max-width: 800px;
+      }
+
+      .sparql-code-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        cursor: copy;
+        font-family: monospace;
+        overflow: auto;
+        max-height: 300px;
+        padding: 10px 20px;
+        scrollbar-width: inherit;
       }
 
       .sparql-code {
         white-space: pre;
-        padding: 10px 20px;
-        cursor: copy;
-        font-family: monospace;
       }
     `
   ]
 
   static properties = {
-    text: { type: String, attribute: false }
+    text: { type: String, attribute: false },
+    arePrefixesVisible: { type: Boolean, state: true },
   }
 
   render() {
@@ -59,11 +72,29 @@ export default class SparqlDialog extends ui.BaseMixin(LitElement) {
           </gscape-button>
         </div>
 
-        <div class="dialog-body">
-          <div class="sparql-code" title="Click to copy query" @click=${this.copyQuery}>${this.text.trim()}</div>
+        <div class="sparql-code-wrapper" title="Click to copy query" @click=${this.copyQuery}>
+          ${this.text === emptyQueryMsg()
+            ? html`<div class="sparql-code">${this.text.trim()}</div>`
+            : html`
+              ${this.arePrefixesVisible
+                ? html`
+                  <div class="sparql-code">${this.queryPrefixes}</div>
+                `
+                : null
+              }
+              <gscape-button type="subtle" title="Show Prefixes" size="s" @click="${this.togglePrefixes}">
+                <span slot="icon">${ellipsis}</span>
+              </gscape-button>
+              <div class="sparql-code">${this.queryText}</div>
+            `
+      }
         </div>
       </div>
     `
+  }
+
+  private togglePrefixes() {
+    this.arePrefixesVisible = !this.arePrefixesVisible
   }
 
 
@@ -76,6 +107,14 @@ export default class SparqlDialog extends ui.BaseMixin(LitElement) {
   protected firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
     super.firstUpdated(_changedProperties)
     this.hide()
+  }
+
+  private get queryPrefixes() {
+    return this.text.substring(0, this.text.search('SELECT')).trim()
+  }
+
+  private get queryText() {
+    return this.text.substring(this.text.search('SELECT')).trim()
   }
 }
 
